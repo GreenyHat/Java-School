f433ba523adfedb378e192bd40815e50403ea650
GreenyHat <aramirezgarcia95@gmail.com>
2023-10-20 13:42:51 +0200

    update-w_FileMethods-added

--- ApuntesConCodigo/src/POO/ServiciosProcesos/CicloDeVidaProcesos.java
+++ ApuntesConCodigo/src/POO/ServiciosProcesos/CicloDeVidaProcesos.java
@@ -1,19 +1,16 @@
 package POO.ServiciosProcesos;
 
-/*
- * 1. El SO crea el proceso y pasa a estado NUEVO (el sistema reserva memoria para el)
- * 
- * 2. Cuando se termina de crear el proceso el SO lo admite y pasa a estado LISTO
- *    En el estado LISTO hay una cola de procesos, a los cuales el planificador del SO 
- *    les asigna un orden de cambio de estado y los va seleccionando para ejecutarse. 
- * 
- * 3. Una vez seleccionados pasan a estado EJECUCION. Puede darse el caso de que se necesite una 
- *    operacion de E/S en cuyo caso pasa al estado BLOQUEADO/ESPERA hasta que recibe la info
- *    haya pedido y entonces se ejecute
- * 
- *    
- *    
- */
 public class CicloDeVidaProcesos {
+    /*
+     * 1. El SO crea el proceso y pasa a estado NUEVO (el sistema reserva memoria para el)
+     * 
+     * 2. Cuando se termina de crear el proceso el SO lo admite y pasa a estado LISTO
+     *    En el estado LISTO hay una cola de procesos, a los cuales el PLANIFICADOR del SO 
+     *    les asigna un orden de cambio de estado y los va seleccionando para ejecutarse. 
+     * 
+     * 3. Una vez seleccionados pasan a estado EJECUCION. Puede darse el caso de que se necesite una 
+     *    operacion de E/S en cuyo caso pasa al estado BLOQUEADO/ESPERA hasta que recibe la info
+     *    haya pedido y entonces se ejecute
+     */
     
 }
